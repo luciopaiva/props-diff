545c50751ce2a15883c6afedccba43f7a2ca5bd4
Lucio Paiva <luciopaiva@gmail.com>
2019-11-05 16:27:13 +0000

    First functional version

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 # props-diff
 
 Compares two files containing properties and shows an HTML summary of the comparison.
+
+Assumptions:
+
+- files are TSV
+- file are encoded in UTF-8
+- columns are not surrounded by quotes
+- first row is **not** headers, but data
--- props-diff.js
+++ props-diff.js
@@ -1,11 +1,79 @@
 
+const
+    fs = require("fs");
+
+let file1 = null;
+let file2 = null;
+let nameColumnIndex = 0;
+let valueColumnIndex = 0;
+
+/** @type {Map<String, [String, String]>} */
+const valuesByPropertyName = new Map();
+
 function main(args) {
-    if (args.length !== 2) {
-        console.error("Must provide two files for comparison.");
-        process.exit(1);
+    if (args.length !== 4) {
+        fatal("Usage: node props-diff <file1> <file2> <name-column-index> <value-column-index>");
+    }
+    [file1, file2, nameColumnIndex, valueColumnIndex] = args;
+
+    processFile(file1, 0);
+    processFile(file2, 1);
+    generateOutputFile();
+}
+
+function processFile(fileName, fileIndex) {
+    const contents = fs.readFileSync(fileName, "utf-8");
+    const lines = contents.split("\n");
+    // if (lines.length === 0) {
+    //     fatal(`File ${fileName} is empty!`);
+    // }
+
+    const minimumExpectedColumnsCount = Math.max(nameColumnIndex, valueColumnIndex) + 1;
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        if (line.length === 0) {
+            continue;  // skip empty line
+        }
+        const columns = line.split("\t");
+        if (columns.length < minimumExpectedColumnsCount) {
+            fatal(`Too few columns in ${fileName}, line ${i + 1}. ` +
+                `Expected ${minimumExpectedColumnsCount} due to column indexes provided.`);
+        }
+        const [name, value] = [columns[nameColumnIndex], columns[valueColumnIndex]];
+
+        let values = valuesByPropertyName.get(name);
+        if (!values) {
+            values = Array(2);
+            values[0] = values[1] = "";
+            valuesByPropertyName.set(name, values);
+        }
+        values[fileIndex] = value.replace(/"/g, "\\\"");
+    }
+}
+
+function generateOutputFile() {
+    const names = Array.from(valuesByPropertyName.keys());
+    names.sort((a, b) => a.localeCompare(b));
+
+    const result = [];
+    for (const name of names) {
+        const [value1, value2] = valuesByPropertyName.get(name);
+        result.push(`"${name}": ["${value1}", "${value2}"],`);
     }
 
+    const templateFile = fs.readFileSync("index.html", "utf-8");
+    const output = templateFile
+        .replace("$RESULT", result.join("\n"))
+        .replace("$FILE1", file1)
+        .replace("$FILE2", file2);
 
+    fs.writeFileSync("output.html", output);
+}
+
+function fatal(msg) {
+    console.error(msg);
+    process.exit(1);
 }
 
 main(process.argv.slice(2));
